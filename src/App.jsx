8f981473fcabe73b4742e67862debf42763a6ca8
kelvineddyimache <ifemicheal2@gmail.com>
2025-07-27 08:12:03 +0100

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Toaster } from "sonner";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import CartSidebar from "./components/Cart/CartSidebar";
-import HomePage from "./pages/HomePage";
-import ShopPage from "./pages/ShopPage";
-import ProductPage from "./pages/ProductPage";
-import CartPage from "./pages/CartPage";
-import CheckoutPage from "./pages/CheckoutPage";
-import AccountPage from "./pages/AccountPage";
-import WishlistPage from "./pages/WishlistPage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import AdminPage from "./pages/AdminPage";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import { CartProvider } from "./lib/cart-context";
-import { AuthProvider } from "./lib/auth-context";
-import { LoyaltyProvider } from "./lib/loyalty-context";
-import PaymentConfirmationPage from "./pages/PaymentConfirmationPage";
-import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <AuthProvider>
-      <LoyaltyProvider>
-        <CartProvider>
-          <div className="min-h-screen flex flex-col">
-            <Header />
-            <ScrollToTop /> {/* ✅ Correct placement here */}
-            <main className="flex-1">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/shop" element={<ShopPage />} />
-                <Route path="/shop/:category" element={<ShopPage />} />
-                <Route path="/product/:slug" element={<ProductPage />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route
-                  path="/confirmation"
-                  element={<PaymentConfirmationPage />}
-                />
-                <Route
-                  path="/account"
-                  element={
-                    <ProtectedRoute requireAuth={true}>
-                      <AccountPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/wishlist"
-                  element={
-                    <ProtectedRoute requireAuth={true}>
-                      <WishlistPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route
-                  path="/admin"
-                  element={
-                    <ProtectedRoute requireAdmin={true}>
-                      <AdminPage />
-                    </ProtectedRoute>
-                  }
-                />
-              </Routes>
-            </main>
-            <Footer />
-            <CartSidebar />
-            <Toaster position="top-right" />
-          </div>
-        </CartProvider>
-      </LoyaltyProvider>
-    </AuthProvider>
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: 32,
+      textAlign: "center"
+    }}>
+      🚧 Site temporarily unavailable.<br />
+      Please check back later.
+    </div>
   );
 }
 
