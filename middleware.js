c72f371c00a1fa7d164564ceb0f97b8f285bfb9a
kelvineddyimache <ifemicheal2@gmail.com>
2025-07-27 07:43:32 +0100

    temp

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   return new NextResponse(
-    '🚧 Site temporarily unavailable. Please check back later.',
+    '🚧 Site temporarily unavailable. Please check back later thanks.',
     { status: 503 }
   );
 }
